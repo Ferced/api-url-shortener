47e5bee227e2aff5c6aa879efe2be2a39c4a2d04
Ferced <fcedeirajoaquin@gmail.com>
2019-06-28 13:49:44 -0300

    se agrego la variable cantidad_de_caracteres

--- url_generator/generadorUrl.py
+++ url_generator/generadorUrl.py
@@ -2,9 +2,10 @@ import random
 class GeneradorUrl:
     def generarUrlAleatoria():
         #GENERA UN CODIGO DE 7 CARACTERES DE BASE64 (ELIGE UNA DE LAS 3 LISTA DE NUMEROS ASCII Y DE ESA ELIJE UNO ALEATORIO) ASI X 7
+        cantidad_de_caracteres=7
         url_corta="http://meli.st/"
         lista_ascii_aceptables=[['0','9'],['A','Z'],['a','z']]
-        for i in range(7):
+        for i in range(cantidad_de_caracteres):
             numero_lista_aleatorio=random.randint(0,len(lista_ascii_aceptables)-1)
             numero_ascii=random.randint(ord(lista_ascii_aceptables[numero_lista_aleatorio][0]),ord(lista_ascii_aceptables[numero_lista_aleatorio][1]))
             url_corta+=chr(numero_ascii)
